0f76fc0881e4c9b968e33fb1831672185f22fb30
IgorGap <ig.gaponenko@gmail.com>
2022-08-30 11:21:07 +0300

    rel.2

--- client/src/components/InfoTable/HostingData.js
+++ client/src/components/InfoTable/HostingData.js
@@ -5,6 +5,40 @@ import * as FaIcons from 'react-icons/fa';
 import * as RiIcons from 'react-icons/ri';
 
 const HostingData = {
-
+  account: 'srv164042 (Бета-SSD)',
+  statistics: {
+    occupied: '675.01 / 7000',
+    cron: 0,
+    MySQL: 1,
+    FTP: 0,
+  },
+  mode: 'включен',
+  actions: {
+    mail: <AiIcons.AiOutlineMail />,
+    users: <FaIcons.FaUsers />,
+    folder: <AiIcons.AiFillFolderOpen />,
+    db: <AiIcons.AiOutlineDatabase />,
+    box: <AiIcons.AiOutlineDropbox />,
+    mailFilter: <RiIcons.RiMailCheckFill />,
+    clock: <AiIcons.AiOutlineClockCircle />,
+  },
+  IP: '31.28.24.122',
+  databaseСapacity: '10.89 Mb',
+  mailCapacity: '522.43 Mb',
+  filesCapacity: '141.7 Mb',
+  comment: '',
+  service: 'Бета-SSD',
+  login: 'user',
+  FTPHostName: 'FTPHostName',
+  FTPPassword: '123',
+  SSHHostName: 'SSHHostName',
+  SSHPassword: '123',
+  MySQL: 'MySQL',
+  PgSQL: 'PgSQL',
+  addresses: [
+    { id: 1, address: 'shoku-yoku.ru' },
+    { id: 2, address: 'srv164042.hoster-test.ru' },
+    { id: 3, address: 'секуеку.рф' },
+  ],
 };
 export default HostingData;
